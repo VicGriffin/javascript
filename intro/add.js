69334cdcbd3fb7055abdb190ca8aa310d3bc2b2b
vicgriffin123 <129837311+vicgriffin123@users.noreply.github.com>
2024-06-17 17:05:37 +0300

    more on js

--- intro/add.js
+++ intro/add.js
@@ -1,11 +1,26 @@
-// let a = 3;
-// let b = 2;
-// const add = (a, b) =>{
-//     return a + b;
-// }
-// console.log(add(a, b));
+let a = 3;
+let b = 2;
+const add = (a, b) =>{
+    return a + b;
+}
+console.log(add(a, b));
 
 const sayHello = function(Name){
     console.log(`Hello, ${Name} i would like you to do some work for me`)
 }
 sayHello("victor");
+
+// arrow function
+const sayGoodnight = (Name) => {
+    console.log(`Goodnight, ${Name} have a good time sleeping`)
+}
+sayGoodnight("victor")
+
+// if function
+let age = 26;
+if ( age >= 18) {
+    console.log(`the person is an adult`)
+}else {
+    console.log(`the person is a minor so no alcohol`);
+}
+
