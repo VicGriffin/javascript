b33d4e979d67e3134a0bccda656b6abfdace75fc
vicgriffin123 <129837311+vicgriffin123@users.noreply.github.com>
2024-06-17 17:32:17 +0300

    more on js

--- intro/add.js
+++ intro/add.js
@@ -23,4 +23,44 @@ if ( age >= 18) {
 }else {
     console.log(`the person is a minor so no alcohol`);
 }
+//if else
+function grade1(es){
+if (es >= 50){
+    return (`A`);
+}else if(es >= 40){
+    return (`B`);
+}else if (es >= 30){
+    return (`C`);
+}else if (es >= 20){
+    return (`D`);
+}else{
+    return (`fail`);
+}
+}
+let english = 40;
+let swahili = 49;
+console.log(`English score is ${english}`)
+console.log(`your english grade is ${grade1(english)}`)
+console.log(`swajili score is ${swahili}`)
+console.log(`your swahili grade is ${grade1(swahili)}`)
+
+
+function grade(m){
+if (m >= 90){
+    return (`A`);
+}else if(m >= 80){
+    return (`B`);
+}else if (m >= 70){
+    return (`C`);
+}else if (es >= 60){
+    return (`D`);
+}else{
+    return (`fail`);
+}
+}
+let maths = 80;
+
+console.log(`you math score is maths:${maths}`)
+console.log(`your math grade is ${grade(maths)}`);
+
 
